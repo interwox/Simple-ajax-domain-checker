5ed62dc5c4ea92522c3a04d206ccc2317c34b0f5
interwox <42099426+interwox@users.noreply.github.com>
2018-08-05 19:00:06 +0200

    Update and rename .gitignore to index.php

---
+++ .gitignore/index.php
@@ -0,0 +1,19 @@
+ <? 
+/********************************************************************************************************************
+* Created by Interwox
+* Website: www.interwox.com
+* License: GNU
+* 
+*********************************************************************************************************************/
+?>
+ <!doctype html>
+<html>
+<head>
+<meta charset="utf-8">
+<title>Simple domain search</title>	  
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.0/jquery.min.js"></script>
+</head>
+<body>                  
+<center><?php include("include/domainsearch.php"); ?></center>
+</body>
+</html>    
